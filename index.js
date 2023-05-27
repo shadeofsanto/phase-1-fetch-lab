1c2650d19777b389968589f201628066badfe1d2
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-05-27 18:44:42 -0500

    Lab fetched

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const firenIce = fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then((json) => renderBooks(json))
+  return firenIce
 }
 
 function renderBooks(books) {
